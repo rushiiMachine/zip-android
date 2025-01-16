9193318cb7822081449f714bd6eb907c3079a418
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2025-01-15 16:19:28 -0800

    fix(testapp): don't use external storage dir

--- app/src/main/kotlin/com/github/diamondminer88/zip/MainActivity.kt
+++ app/src/main/kotlin/com/github/diamondminer88/zip/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.github.diamondminer88.zip
 
-import android.Manifest
 import android.app.Activity
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Environment
 import android.util.Log
-import androidx.core.app.ActivityCompat
 import java.io.File
 import kotlin.system.measureNanoTime
 
@@ -16,12 +12,7 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-            return requestPermissions()
-
-        val baseDir = File(Environment.getExternalStorageDirectory(), "/zip-android")
-        val zipFile = File(baseDir, "testzip.zip")
-        baseDir.mkdir()
+        val zipFile = File(this.externalCacheDir, "modified.zip")
         zipFile.createNewFile()
         zipFile.writeBytes(resources.openRawResource(R.raw.testzip).readBytes())
 
@@ -63,18 +54,5 @@ class MainActivity : Activity() {
 
             zip.forEach { println("Entry: ${it.name} compressed size: ${it.compressedSize} raw size: ${it.size}") }
         }
-    }
-
-    private fun requestPermissions() {
-        val requestId = 1
-        val storagePerms = arrayOf(
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
-        ActivityCompat.requestPermissions(
-            this,
-            storagePerms,
-            requestId
-        )
     }
 }
